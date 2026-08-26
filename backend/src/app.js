828c9a01dc3f3591a37e20fbfdd56c2acb4df278
Shreya <shreya28022005@gmail.com>
2026-08-26 11:51:31 +0530

    Fix CORS configuration

--- backend/src/app.js
+++ backend/src/app.js
@@ -9,7 +9,7 @@ const songRoutes = require("./routes/songs.routes");
 
 app.use(cors({
     origin:  [
-        "http://localhost:5174"
+        "http://localhost:5173"
     ],
     credentials: true
 }));
